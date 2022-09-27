4edcbed3b7f5e0137cb397765dfad02f2b1c2041
Calvin Wallis <calvinwallis@yahoo.com>
2022-09-26 23:13:18 -0700

    Fill out main menu, get it working, add arrows

--- src/app/pages/main-menu/main-menu.component.ts
+++ src/app/pages/main-menu/main-menu.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { MenuItemDirective } from 'src/app/directives/menu-item.directive';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent implements OnInit {
+export class MainMenuComponent implements OnInit, AfterViewInit {
+  @ViewChildren(MenuItemDirective) menuItems: QueryList<MenuItemDirective> = new QueryList<MenuItemDirective>;
 
-  constructor() { }
+  constructor(private menuService: MenuService) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.menuService.setMenuItems(this.menuItems);
+    this.menuService.subscribeToMenuNavigation();
+
+    requestAnimationFrame(() => {
+      this.menuItems.first.element.focus();
+    })
+  }
 }
