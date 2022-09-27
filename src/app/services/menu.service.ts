3c30855cba28f405766144b57d5264513bea39a4
Calvin Wallis <calvinwallis@yahoo.com>
2022-09-26 22:45:51 -0700

    move menu logic into menu service

---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, OnDestroy, QueryList } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { MenuItemDirective } from '../directives/menu-item.directive';
+import { KeyboardService } from './keyboard.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService implements OnDestroy {
+  #subscriptions: Subscription = new Subscription();
+  menuItems: BehaviorSubject<QueryList<MenuItemDirective>> = new BehaviorSubject<QueryList<MenuItemDirective>>(new QueryList<MenuItemDirective>());
+
+  constructor(private keyboardService: KeyboardService) { }
+
+  subscribeToMenuNavigation() {
+    this.#subscriptions.add(
+      this.keyboardService.observeArrowKeys().subscribe((result) => {
+        const items = this.menuItems.getValue().toArray();
+        const key = (result as KeyboardEvent).key;
+        let activeIndex = items.findIndex(
+          (element) => element.element === document.activeElement
+        );
+        if (key === 'ArrowUp' && activeIndex > 0) {
+          activeIndex--;
+        } else if (key === 'ArrowDown' && activeIndex < items.length - 1) {
+          activeIndex++;
+        }
+        items[activeIndex].element.focus();
+      })
+    );    
+  }
+
+  setMenuItems(menuItems: QueryList<MenuItemDirective>): void {
+    this.menuItems.next(menuItems);
+  }
+
+  ngOnDestroy(): void {
+    this.#subscriptions.unsubscribe();
+  }
+}
