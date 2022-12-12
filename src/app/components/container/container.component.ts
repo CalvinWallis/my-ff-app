dd7c9e988c4317125a799c3141c87adc1a73eba4
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-11 23:28:17 -0800

    add auto-height input to app-container component

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent implements OnInit {
+  @Input() autoHeight: boolean = false;
 
   constructor() { }
 
