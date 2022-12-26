45ae9befd2602340d6d1ee30bd1627146333a100
Calvin Wallis <calvinwallis@yahoo.com>
2022-12-25 23:13:19 -0800

    only subscribe to MenuNavigation if it hasnt happened yet.

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -8,31 +8,38 @@ import { KeyboardService } from './keyboard.service';
 })
 export class MenuService implements OnDestroy {
   #subscriptions: Subscription = new Subscription();
+  menuNavigationStarted: boolean = false;
   menuItems: BehaviorSubject<QueryList<MenuItemDirective>> = new BehaviorSubject<QueryList<MenuItemDirective>>(new QueryList<MenuItemDirective>());
 
   constructor(private keyboardService: KeyboardService) { }
 
   subscribeToMenuNavigation() {
-    this.#subscriptions.add(
-      this.keyboardService.observeArrowKeys().subscribe((result) => {
-        const items = this.menuItems.getValue().toArray();
-        const key = (result as KeyboardEvent).key;
-        let activeIndex = items.findIndex(
-          (element) => element.element === document.activeElement
-        );
-        if (key === 'ArrowUp' && activeIndex > 0) {
-          activeIndex--;
-        } else if (key === 'ArrowDown' && activeIndex < items.length - 1) {
-          activeIndex++;
-        }
-        items[activeIndex].element.focus();
-      })
-    );    
+    if(!this.menuNavigationStarted){
+      this.#subscriptions.add(
+        this.keyboardService.observeArrowKeys().subscribe((result) => {
+          const items = this.menuItems.getValue().toArray();
+          const key = (result as KeyboardEvent).key;
+          let activeIndex = items.findIndex(
+            (element) => element.element === document.activeElement
+          );
+          if (key === 'ArrowUp' && activeIndex > 0) {
+            activeIndex--;
+          } else if (key === 'ArrowDown' && activeIndex < items.length - 1) {
+            activeIndex++;
+          }
+          items[activeIndex].element.focus();
+        })
+      );    
+
+      this.menuNavigationStarted = true;
+    }
   }
 
   setMenuItems(menuItems: QueryList<MenuItemDirective>): void {
     this.menuItems.next(menuItems);
   }
+
+  
 
   ngOnDestroy(): void {
     this.#subscriptions.unsubscribe();
